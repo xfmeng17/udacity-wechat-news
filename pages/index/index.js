19bfcd7e2264c13d7793171be05237fef0cc964e
xfmeng17 <xfmeng17@foxmail.com>
2019-04-20 23:06:59 +0800

    feat(index): finish getNewsList() and related function support
    Ignore the hot news in the top cause I don't want spend to much
    time on HTML/CSS.

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,17 +9,52 @@ Page({
       { value: 'ty', label: '体育' },
       { value: 'other', label: '其他' },
     ],
-    currChannel: 'gn',
+    channel: 'gn',
+    newsList: [],
   },
-  changeChannel(e) {
-    // console.log(e)
+  onLoad () {
+    this.getNewsList()
+  },
+  onTapChannel(e) {
     let channel = e.currentTarget.dataset.channel
     this.setData({
-      currChannel: channel
+      channel: channel
     })
-    this.getNews(channel)
+    this.getNewsList()
   },
-  getNews (channel) {
-    console.log('channel = ', channel)
-  }
+  getNewsList (callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.channel
+      },
+      success: res => {
+        let result = res.data.result
+        result.forEach(item => {
+          let date = new Date(item.date);
+          let hour = (date.getHours() < 10 ? '0' : '') + date.getHours()
+          let minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
+          item.time = hour + ':' + minute 
+          let source = item.source.split('/')
+          item.cp = source.length > 0 ? source[0] : item.source
+          let firstImage = item.firstImage
+          if (item.firstImage.indexOf('http:') == -1) {
+            item.firstImage = 'https:' + item.firstImage
+          }
+        })
+        console.log(result)
+        this.setData({
+          newsList: result
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+  onPullDownRefresh () {
+    this.getNewsList(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
 })
